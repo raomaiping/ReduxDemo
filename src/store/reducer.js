c73f1664574d37b012bc5194e0959c54de1c7d90
raomaiping <2582395486@qq.com>
2020-03-10 16:54:18 +0800

    创建Redux中的仓库-store 和 reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue:'Write Someting',
+    list:[
+        '早上八点开晨会，分配今天的代码任务',
+        '早九点和项目经理沟通会',
+        '早上八点开晨会，分配今天的代码任务'
+    ]
+}
+
+export default (state = defaultState,action)=>{
+    return state
+}
