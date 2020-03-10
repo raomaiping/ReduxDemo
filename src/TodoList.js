c2213233c62b9f20e43dac4a14178032af654e1c
raomaiping <2582395486@qq.com>
2020-03-10 21:18:17 +0800

    Reducx 小技巧2

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Input , Button ,List} from 'antd'
 import 'antd/dist/antd.css' 
 import store from './store'
-import {CHANGE_INPUT,ADD_ITEM,DELETE_ITEM} from './store/actionTypes'
+import {changeInputAction,addItemAction,deleteItemAction} from './store/actionCreators'
 class TodoList extends Component {
 
     constructor(props){
@@ -40,11 +40,7 @@ class TodoList extends Component {
     }
 
     changeInputValue(e){
-        const action = {
-            type:CHANGE_INPUT,
-            value:e.target.value
-        }
-        
+        const action = changeInputAction(e.target.value)
         store.dispatch(action)
     }
 
@@ -52,14 +48,11 @@ class TodoList extends Component {
         this.setState(store.getState())
     }
     clickBtn(){
-        const action = {type:ADD_ITEM,}
+        const action = addItemAction()
         store.dispatch(action)  
     }
     deleteItem(index){
-        const action ={
-            type:DELETE_ITEM,
-            index
-        }
+        const action = deleteItemAction(index)
         store.dispatch(action)
     }
 }
